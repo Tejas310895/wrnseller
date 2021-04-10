db150bf20f35588e7f8f43d9eb4f7edcd88160ce
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-04-10 16:00:17 +0530

    error

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
   <link rel="icon" type="image/png" href="images/karwarslogo.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-    Karwars Seller Panel
+    Wernear Seller Panel
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
